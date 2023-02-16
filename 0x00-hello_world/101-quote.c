ac00da7b891e9fd026ac1d9c8e854bde40759efe
naijmontana <rahmasuleimanbaba@gmail.com>
2023-02-16 06:38:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - prints to string
  *
@@ -9,6 +8,16 @@
  */
 int main(void)
 {
-	fput("and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long l = 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
+
+	__asm__ ("syscall"
+			: "=a" (ret)
+			: "a" (syscall),
+			"D" (fd),
+			"S" (s),
+			"d" (l));
 	return (1);
-}
